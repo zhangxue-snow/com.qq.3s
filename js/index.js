5c0135bee5cc8eb57baa1b69a4f3f535fe26a828
zhangxue-snow <1539959785@qq.com>
2020-04-23 18:31:26 +0800

    提交代码

--- js/index.js
+++ js/index.js
@@ -5,6 +5,32 @@ $(function () {
 
     })
 
+    // 登录弹窗
+    // 1、点击登录按钮显示
+    $('#kv-loginbtn').on('click', function () {
+        $('.login-window').css('visibility', 'visible')
+    })
+    // 2、点击关闭按钮隐藏
+    $('.login-close').on('click', function () {
+        $('.login-window').css('visibility', 'hidden')
+    })
+
+    // $('#kv-loginbtn').mouseenter(function () {
+    //     $('.login-window').css('visibility', 'visible')
+    // })
+    // // 2、点击关闭按钮隐藏
+    // $('#kv-loginbtn').mouseleave(function () {
+    //     $('.login-window').css('visibility', 'hidden')
+    // })
+
+    // $('#kv-loginbtn').mouseenter(function () {
+    //     $('.login-window').show()
+    // })
+    // // 2、点击关闭按钮隐藏
+    // $('#kv-loginbtn').mouseleave(function () {
+    //     $('.login-window').hide()
+    // })
+
     // npc-tab切换
     $('.item1').hide()
     // 1、给li添加点击事件
@@ -20,10 +46,59 @@ $(function () {
 
 
     // 轮播图
+    // 把每张图片的位置效果（css）放在一个数组（arr）里，到时候通过点击左右按钮来改变顺序，然后再把图片放到对应的位置即可
     var arr = [
-        {
-            // 1
-            width:
-        }
+        {// 1
+            width: 790.64,
+            height: 423.56,
+            top: 30,
+            left: -10,
+            zIndex: 2
+        },
+        {// 2
+            width: 895.93,
+            height: 479.96,
+            top: -2,
+            left: 90,
+            zIndex: 3
+        },
+        {// 4
+            width: 707.33,
+            height: 380.5,
+            top: 30,
+            right: -10,
+            zIndex: 1
+        },
+        {// 3
+            width: 790.64,
+            height: 423.56,
+            top: 30,
+            right: -10,
+            zIndex: 2
+        },
     ]
+
+    // 让每个li标签走到指定位置
+
+    assign();
+    function assign() {
+        $.each($('.slide'), function (index, val) {
+            // index是索引号 ele是数组中的对象
+            $(val).animate(arr[index])
+            // console.log(arr[index]);
+        })
+    }
+
+
+    // 给左侧按钮添加点击事件
+    $('.prev').on("click", function () {
+        arr.unshift(arr.pop())
+        assign();
+    })
+
+    // 给右侧按钮添加点击事件
+    $('.next').on("click", function () {
+        arr.push(arr.shift())
+        assign();
+    })
 })
